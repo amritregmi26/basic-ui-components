53dcdd325aeb1ac102b00b8891f179a205844082
amritregmi26 <amrregmi@gmail.com>
2023-12-04 20:29:55 +0545

    hero provisional card updated

---
+++ src/components/Heros/components/provisionalCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProvisionalCard = ({image, title}) => {
+  return (
+    <div className="px-5 py-5 flex flex-col gap-4 bg-white shadow">
+        <img className="w-full h-10 border-2 border-[#000]" src="" alt="Hero image" />
+        <p>title</p>
+    </div>
+  )
+}
+
+export default ProvisionalCard
--- src/components/Heros/hero.jsx
+++ src/components/Heros/hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Header from '../Global/header'
+import ProvisionalCard from './components/provisionalCard'
 
 export const Hero = () => {
   return (
-    <Header heading="Hero-Sections" />
+    <>
+      <Header heading="Hero-Sections" />
+
+      <div className=" mt-12 container flex wrap align-center justify-around">
+        <ProvisionalCard image="" title = ""/>
+      </div>
+    </>
   )
 }
